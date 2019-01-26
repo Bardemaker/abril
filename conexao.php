5c84cb8a8b36bc21cb8db38760f4f943eb3e0945
Bardemaker <hbaneto@gmail.com>
2019-01-26 22:51:29 +0000

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,18 +1,14 @@
 <?php
 
 $servidor = "localhost";
-$login = "bardemaker_abril";
-$senha = "X{-;z9f(VRPz";
-$banco = "bardemaker_abril";
+$login = "root";
+$senha = "";
+$banco = "abril";
 $con = mysql_connect($servidor, $login, $senha);
 
 if($con != NULL)
 {
-$db = mysql_select_db($banco);
-	/*if($db != NULL)
-		echo "Sucesso!";	
- 	else 
-		echo "Erro no servidor";*/	
+	$db = mysql_select_db($banco);	
 }
 
 include "common.php";
